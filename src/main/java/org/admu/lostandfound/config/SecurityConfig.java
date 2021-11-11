118e86c6f8cd1397fee32d0e7e36f5f2dc643387
Ian Red D. Ligot <redligot2009.forreal@gmail.com>
2021-11-11 16:00:52 +0800

    Fix Spring Security Config to allow for PUT / POST / DELETE

--- src/main/java/org/admu/lostandfound/config/SecurityConfig.java
+++ src/main/java/org/admu/lostandfound/config/SecurityConfig.java
@@ -9,7 +9,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll().and()
+                .cors().and()
+                .csrf().disable();
     }
 
 }

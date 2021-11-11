6180c5feeb6f9d4bc9eb662ce8cd94c13ba9e1f2
Ian Red D. Ligot <redligot2009.forreal@gmail.com>
2021-11-11 15:05:45 +0800

    Fix Jersey Config in order to find controllers

---
+++ src/main/java/org/admu/lostandfound/config/JerseyConfig.java
@@ -0,0 +1,15 @@
+package org.admu.lostandfound.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+
+        // REQUIRED:
+        // need to explicitly tell Jersey where the REST classes are found
+        packages("org.admu.lostandfound.controllers");
+    }
+}

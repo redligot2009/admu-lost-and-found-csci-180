e194c12a4472ea28d9307fe330e432f8e2375895
Renzo Tan <renzotimtan@gmail.com>
2021-11-11 16:05:54 +0800

    fixed category repo bug

--- src/main/java/org/admu/lostandfound/repositories/CategoryRepository.java
+++ src/main/java/org/admu/lostandfound/repositories/CategoryRepository.java
@@ -1,11 +1,10 @@
 package org.admu.lostandfound.repositories;
 
 import org.admu.lostandfound.models.Category;
-import org.admu.lostandfound.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<User, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     public Category findByTitle(String title);
 }

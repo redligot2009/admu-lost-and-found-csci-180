294ca8bcd3ad2479c7c696d701d53b25a65d8c61
Darren Ting <darren.ting@obf.ateneo.edu>
2021-12-16 19:17:04 +0800

    ClaimsController has GET, partially made POST, but no DELETE. ClaimsComponent has changes in comments to support the POST

--- src/main/java/org/admu/lostandfound/components/ClaimsComponent.java
+++ src/main/java/org/admu/lostandfound/components/ClaimsComponent.java
@@ -3,14 +3,22 @@ package org.admu.lostandfound.components;
 import org.admu.lostandfound.models.Claim;
 import org.admu.lostandfound.models.LostItem;
 import org.admu.lostandfound.models.User;
+import org.admu.lostandfound.repositories.ClaimRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
 
 @Component
 public class ClaimsComponent 
 {
 	
 	// Repositories @Autowire here
-	
+	@Autowired
+	private ClaimRepository claimRepo;
+
 	
 	public Claim createNewClaim(User claimer, LostItem lostItem)
 	{
@@ -36,5 +44,22 @@ public class ClaimsComponent
 	{
 		return null;
 	}
-	
+
+//	public Claim newClaim(Map<String, Object> body) {
+//
+//		// Dates - default is now
+//		LocalDate created_date = LocalDate.now();
+//		System.out.println(created_date);
+//
+//		// Foreign key
+//		Long claimer_id = Long.valueOf(body.get("locationId").toString());
+//
+//		Long lost_item = Long.valueOf(body.get("lost item").toString());
+//
+//		// Save new lost item
+//		Claim Claim = new Claim(claimer_id, lost_item, created_date);
+//		claimRepo.save(lostItem);
+//		return lostItem;
+//	}
+
 }
---
+++ src/main/java/org/admu/lostandfound/rest/controllers/ClaimsController.java
@@ -0,0 +1,56 @@
+package org.admu.lostandfound.rest.controllers;
+
+import org.admu.lostandfound.components.ClaimsComponent;
+import org.admu.lostandfound.models.Claim;
+import org.admu.lostandfound.repositories.ClaimRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Map;
+
+@Component
+@Path("/Claims")
+public class ClaimsController
+{
+    @Autowired
+    ClaimsComponent claimsComp;
+
+    @Autowired
+    ClaimRepository claimRepo;
+
+    @GET
+    @Path("/claiming")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getClaims(@QueryParam("item_id") Long item,
+                            @QueryParam("user_id") Long claimer)
+    {
+        return "JSON";
+    }
+
+//    @DELETE
+//    @Path("/claims/{id}")
+//    {
+//
+//    }
+
+//    @POST
+//    @Path("/claims")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Claim newClaim(@RequestBody Map<String,Object> body
+//    ) {
+//        return claimsComp.newClaim(body);
+//    }
+
+    @GET
+    @Path("/my_claims")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getOngoingClaims(@QueryParam("item_id") Long item)
+    {
+        return "JSON of Ongoing Claims";
+    }
+
+}

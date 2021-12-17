cdd0c50c43ef03dd7682730ba41619d764302569
Ian Red D. Ligot <redligot2009.forreal@gmail.com>
2021-12-17 15:58:16 +0800

    Add JWT config to application properties + SecurityConfig.java

--- src/main/java/org/admu/lostandfound/config/SecurityConfig.java
+++ src/main/java/org/admu/lostandfound/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package org.admu.lostandfound.config;
 
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        // securedEnabled = true,
+        // jsr250Enabled = true,
+        prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
